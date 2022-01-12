0d50a7e5c4f1a179d9587478a285f230214c1f1c
Jokie <leungjokie@gmail.com>
2022-01-12 03:42:37 +0000

    transPCC exp first init.

--- Dockerfile
+++ Dockerfile
@@ -10,42 +10,98 @@ ENV PYTHONPATH=/depoco/submodules/ChamferDistancePytorch/
 # Provide a data directory to share data across docker and the host system
 RUN mkdir -p /data
 
+# mirrors.aliyun.com / mirrors.tuna.tsinghua.edu.cn
+
+# RUN sed -i s@/archive.ubuntu.com/@/mirrors.aliyun.com/@g /etc/apt/sources.list 
+RUN rm -rf /var/lib/apt/lists/* && apt-get clean
+RUN sed -i s@/archive.ubuntu.com/@/mirrors.aliyun.com/@g /etc/apt/sources.list 
+# RUN sed -i -re 's/de.archive.ubuntu.com/archive.ubuntu.com/g' /etc/apt/sources.list
+# RUN apt-get update
+
+RUN apt-get clean && apt-get update && apt-get install -y software-properties-common 
+# RUN add-apt-repository ppa:deadsnakes/ppa && apt-get update 
+
+# RUN apt-get install -y software-properties-common 
+    # && add-apt-repository ppa:deadsnakes/ppa && apt-get update 
+
 # Install system packages
-RUN apt-get update && apt-get install --no-install-recommends -y \
-    build-essential \
-    cmake \
-    git \
-    libeigen3-dev \
-    libgl1-mesa-glx \
-    libusb-1.0-0-dev \
-    ninja-build \
-    pybind11-dev \
-    python3 \
-    python3-dev \
-    python3-pip \
-    vim \
-    && rm -rf /var/lib/apt/lists/*
+RUN rm -rf /var/lib/apt/lists/* && apt-get clean && apt-get update 
+RUN apt-get clean && apt-get update && apt-get install --no-install-recommends -y \
+    build-essential 
+
+RUN apt-get install --no-install-recommends -y \
+    cmake 
+RUN apt-get install --no-install-recommends -y \
+    git     
+RUN apt-get install --no-install-recommends -y \
+    libeigen3-dev 
+RUN apt-get install --no-install-recommends -y \
+    libgl1-mesa-glx 
+RUN apt-get install --no-install-recommends -y \
+    libusb-1.0-0-dev 
+RUN apt-get install --no-install-recommends -y \
+    ninja-build 
+RUN apt-get install --no-install-recommends -y \
+    pybind11-dev 
+RUN apt-get install --no-install-recommends -y \
+    python3 
+RUN apt-get install --no-install-recommends -y \
+    python3-dev 
+RUN apt-get install --no-install-recommends -y \
+    python3-pip 
+RUN apt-get install --no-install-recommends -y \
+    vim
+RUN rm -rf /var/lib/apt/lists/*
 
 # Install Pytorch with CUDA 11 support
+# RUN pip3 install \
+#     torch==1.7.1 \
+#     -f https://download.pytorch.org/whl/torch_stable.html
+# RUN pip3 install \    
+#     torchvision==0.8.2+cu110 \
+#     torchaudio==0.7.2 \
+#     -f https://download.pytorch.org/whl/torch_stable.html
+
 RUN pip3 install \
-    torch==1.7.1+cu110 \
-    torchvision==0.8.2+cu110 \
-    torchaudio==0.7.2 \
-    -f https://download.pytorch.org/whl/torch_stable.html
+    numpy  -i https://pypi.tuna.tsinghua.edu.cn/simple
+
+
+# Install Pytorch with CUDA 11 support
+RUN pip3 install torch===1.7.1+cu110 -f https://download.pytorch.org/whl/torch_stable.html -i https://mirrors.aliyun.com/pypi/simple/ some-package
+
+RUN pip3 install torchvision===0.8.2+cu110 torchaudio==0.7.2 -f https://download.pytorch.org/whl/torch_stable.html -i https://mirrors.aliyun.com/pypi/simple/ some-package
+
+# RUN pip3 install \
+#     torch==1.7.1 
+#     # -f https://download.pytorch.org/whl/torch_stable.html
+# RUN pip3 install \    
+#     torchvision==0.8.2+cu110 \
+#     torchaudio==0.7.2 
+#     # -f https://download.pytorch.org/whl/torch_stable.html
 
 # Install python dependencies
 RUN pip3 install \
     open3d  \
     tensorboard \
     ruamel.yaml \
-    jupyterlab
+    jupyterlab \
+    -i https://pypi.tuna.tsinghua.edu.cn/simple
+
+# RUN pip3 config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+
+
 
 # Copy the libary to the docker image
 COPY ./ depoco/
 
 # Install depoco and 3rdparty dependencies
 RUN cd depoco/ && pip3 install -U -e .
+
 RUN cd depoco/submodules/octree_handler && pip3 install -U .
+
+ENV TORCH_CUDA_ARCH_LIST="7.5"
 RUN cd depoco/submodules/ChamferDistancePytorch/chamfer3D/ && pip3 install -U . 2>/dev/null
+
+
 
 WORKDIR /depoco/depoco
--- depoco/architectures/network_blocks.py
+++ depoco/architectures/network_blocks.py
@@ -9,6 +9,53 @@ import depoco.architectures.original_kp_blocks as o_kp_conv
 ############ NETWORK BLOCKS Dictionary############
 ##################################################
 
+from pointnet_util import index_points, square_distance, PointNetFeaturePropagation, PointNetSetAbstraction
+#from pointnet_util import PointNetFeaturePropagation, PointNetSetAbstraction
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+class TransformerBlock(nn.Module):
+    def __init__(self, d_points, d_model, k) -> None:
+        super().__init__()
+        self.fc1 = nn.Linear(d_points, d_model)
+        self.fc2 = nn.Linear(d_model, d_points)
+        self.fc_delta = nn.Sequential(
+            nn.Linear(3, d_model),
+            nn.ReLU(),
+            nn.Linear(d_model, d_model)
+        )
+        self.fc_gamma = nn.Sequential(
+            nn.Linear(d_model, d_model),
+            nn.ReLU(),
+            nn.Linear(d_model, d_model)
+        )
+        self.w_qs = nn.Linear(d_model, d_model, bias=False)
+        self.w_ks = nn.Linear(d_model, d_model, bias=False)
+        self.w_vs = nn.Linear(d_model, d_model, bias=False)
+        self.k = k
+        
+    # xyz: b x n x 3, features: b x n x f
+    def forward(self, xyz, features):
+        dists = square_distance(xyz, xyz)
+        knn_idx = dists.argsort()[:, :, :self.k]  # b x n x k
+        knn_xyz = index_points(xyz, knn_idx)
+        
+        pre = features
+        x = self.fc1(features)
+        q, k, v = self.w_qs(x), index_points(self.w_ks(x), knn_idx), index_points(self.w_vs(x), knn_idx)
+
+        pos_enc = self.fc_delta(xyz[:, :, None] - knn_xyz)  # b x n x k x f
+        
+        attn = self.fc_gamma(q[:, :, None] - k + pos_enc)
+        attn = F.softmax(attn / np.sqrt(k.size(-1)), dim=-2)  # b x n x k x f
+        
+        res = torch.einsum('bmnf,bmnf->bmf', attn, v + pos_enc)
+        res = self.fc2(res) + pre
+        return res, attn
+
+
 def printNan(bla: torch.tensor, pre=''):
     if (bla != bla).any():
         print(pre, 'NAN')
@@ -328,4 +375,133 @@ class AdaptiveDeconv(nn.Module):
         input_dict['points'] = p_new
         input_dict['features'] = f_new
         return input_dict
+
+#------------------------------- point transformer part by Jokie 220111
+class TransitionDown(nn.Module):
+    def __init__(self, k, nneighbor, channels):
+        super().__init__()
+        self.sa = PointNetSetAbstraction(k, 0, nneighbor, channels[0], channels[1:], group_all=False, knn=True)
+
+    def forward(self, xyz, points):
+        return self.sa(xyz, points)
+
+
+class TransitionUp(nn.Module):
+    def __init__(self, dim1, dim2, dim_out):
+        class SwapAxes(nn.Module):
+            def __init__(self):
+                super().__init__()
+
+            def forward(self, x):
+                return x.transpose(1, 2)
+
+        super().__init__()
+        self.fc1 = nn.Sequential(
+            nn.Linear(dim1, dim_out),
+            SwapAxes(),
+            nn.BatchNorm1d(dim_out),  # TODO
+            SwapAxes(),
+            nn.ReLU(),
+        )
+        self.fc2 = nn.Sequential(
+            nn.Linear(dim2, dim_out),
+            SwapAxes(),
+            nn.BatchNorm1d(dim_out),  # TODO
+            SwapAxes(),
+            nn.ReLU(),
+        )
+        self.fp = PointNetFeaturePropagation(-1, [])
+
+    def forward(self, xyz1, points1, xyz2, points2):
+        feats1 = self.fc1(points1)
+        feats2 = self.fc2(points2)
+        feats1 = self.fp(xyz2.transpose(1, 2), xyz1.transpose(1, 2), None, feats1.transpose(1, 2)).transpose(1, 2)
+        return feats1 + feats2
+
+
+class Backbone(nn.Module):
+    def __init__(self, cfg: dict):
+        super().__init__()
+        npoints, nblocks, nneighbor, d_points, transformer_dim = cfg['num_point'], cfg['nblocks'], cfg['nneighbor'], cfg['input_dim'], cfg['transformer_dim']
+        self.fc1 = nn.Sequential(
+            nn.Linear(d_points, 32),
+            nn.ReLU(),
+            nn.Linear(32, 32)
+        )
+        self.transformer1 = TransformerBlock(32, transformer_dim, nneighbor)
+        self.transition_downs = nn.ModuleList()
+        self.transformers = nn.ModuleList()
+        for i in range(nblocks):
+            channel = 32 * 2 ** (i + 1)
+            self.transition_downs.append(TransitionDown(npoints // 4 ** (i + 1), nneighbor, [channel // 2 + 3, channel, channel]))
+            self.transformers.append(TransformerBlock(channel, transformer_dim, nneighbor))
+        self.nblocks = nblocks
+
+    def forward(self, x):
+        xyz = x[..., :3]
+        points = self.transformer1(xyz, self.fc1(x))[0]
+
+        xyz_and_feats = [(xyz, points)]
+        for i in range(self.nblocks):
+            xyz, points = self.transition_downs[i](xyz, points)
+            points = self.transformers[i](xyz, points)[0]
+            xyz_and_feats.append((xyz, points))
+        return points, xyz_and_feats
+
+
+class PointTransformerCls(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.backbone = Backbone(cfg)
+        npoints, nblocks, nneighbor, n_c, d_points = cfg.num_point, cfg.model.nblocks, cfg.model.nneighbor, cfg.num_class, cfg.input_dim
+        self.fc2 = nn.Sequential(
+            nn.Linear(32 * 2 ** nblocks, 256),
+            nn.ReLU(),
+            nn.Linear(256, 64),
+            nn.ReLU(),
+            nn.Linear(64, n_c)
+        )
+        self.nblocks = nblocks
+
+    def forward(self, x):
+        points, _ = self.backbone(x)
+        res = self.fc2(points.mean(1))
+        return res
+
+
+class PointTransformerPCC(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.backbone = Backbone(cfg)
+        #npoints, nblocks, nneighbor, n_c, d_points = cfg.num_point, cfg.model.nblocks, cfg.model.nneighbor, cfg.num_class, cfg.input_dim
+        npoints, nblocks, nneighbor, n_c, d_points = 1024,  
+        self.fc2 = nn.Sequential(
+            nn.Linear(32 * 2 ** nblocks, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 32 * 2 ** nblocks)
+        )
+        self.transformer2 = TransformerBlock(32 * 2 ** nblocks, cfg.model.transformer_dim, nneighbor)
+        self.nblocks = nblocks
+        self.transition_ups = nn.ModuleList()
+        self.transformers = nn.ModuleList()
+        for i in reversed(range(nblocks)):
+            channel = 32 * 2 ** i
+            self.transition_ups.append(TransitionUp(channel * 2, channel, channel))
+            self.transformers.append(TransformerBlock(channel, cfg.model.transformer_dim, nneighbor))
+
+    def forward(self, x):
+        points, xyz_and_feats = self.backbone(x)
+        xyz = xyz_and_feats[-1][0]
+        points = self.transformer2(xyz, self.fc2(points))[0]
+
+        for i in range(self.nblocks):
+            points = self.transition_ups[i](xyz, points, xyz_and_feats[- i - 2][0], xyz_and_feats[- i - 2][1])
+            xyz = xyz_and_feats[- i - 2][0]
+            points = self.transformers[i](xyz, points)[0]
+
+        return points
+
+
 
